6c47adbeca512bd2a657f895fb841679c4cb59ba
borisbonchev <b.bonchev@student.fontys.nl>
2024-01-07 21:08:28 +0100

    fix: functional logging no more prints

---
+++ lib/services/logging.dart
@@ -0,0 +1,18 @@
+import 'package:logging/logging.dart';
+
+final Logger logger = Logger('MyApp');
+
+// ignore_for_file: avoid_print
+void setupLogger() {
+  Logger.root.level = Level.ALL;
+
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}'); // Output log messages to the console
+    if (rec.error != null) {
+      print('Error: ${rec.error}');
+    }
+    if (rec.stackTrace != null) {
+      print('Stack trace: ${rec.stackTrace}');
+    }
+  });
+}
