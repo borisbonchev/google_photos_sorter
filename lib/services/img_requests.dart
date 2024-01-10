a8afd208c514272d6abbb7bf089e98ade5455f67
borisbonchev <b.bonchev@student.fontys.nl>
2024-01-10 18:26:00 +0100

    feat: returns the authorized google photos link of filtered imgs

--- lib/services/img_requests.dart
+++ lib/services/img_requests.dart
@@ -47,49 +47,57 @@ class PhotoRequests {
     List<String> photoList = await getAllPhotos();
     List<String> albumList = await _albumService.getAlbumIds();
 
-    int count = 1;
-    for (String albumId in albumList) {
-      String jsonBody = jsonEncode({
-        "albumId": albumId,
-        "pageSize": 100,
-      });
-
-      var response = await authClient.post(
-          Uri.parse(
-              'https://photoslibrary.googleapis.com/v1/mediaItems:search'),
-          body: jsonBody);
-
-      if (response.statusCode != 200) {
-        _logger.warning('Failed to get albums: ${response.statusCode}');
-      }
+    if (albumList.isEmpty) {
+      var response = await authClient.get(
+        Uri.parse('https://photoslibrary.googleapis.com/v1/mediaItems'),
+      );
 
       var data = jsonDecode(response.body);
-      if (data['mediaItems'] == null) {
-        _logger.info('No media items found in the album: $albumId');
-        continue;
-      }
       var photos = data['mediaItems'] as List;
-      var photoIds = photos.map((photo) => photo['id'] as String).toList();
+      photoList = photos.map((photo) => photo['id'] as String).toList();
+    } else {
+      for (String albumId in albumList) {
+        String jsonBody = jsonEncode({
+          "albumId": albumId,
+          "pageSize": 100,
+        });
 
-      _logger.info('Album $count');
-      _logger.info('PhotoIds:');
-      _logger.info(photoIds);
+        var response = await authClient.post(
+          Uri.parse(
+              'https://photoslibrary.googleapis.com/v1/mediaItems:search'),
+          body: jsonBody,
+        );
 
-      // Removing photoIds that exist in photoList
-      photoList.removeWhere((photoId) => photoIds.contains(photoId));
-      count++;
+        if (response.statusCode != 200) {
+          _logger.warning('Failed to get albums: ${response.statusCode}');
+        }
+
+        var data = jsonDecode(response.body);
+        if (data['mediaItems'] == null) {
+          _logger.info('No media items found in the album: $albumId');
+          continue;
+        }
+
+        var photos = data['mediaItems'] as List;
+        var photoIds = photos.map((photo) => photo['id'] as String).toList();
+
+        // Removing photoIds that exist in photoList
+        photoList.removeWhere((photoId) => photoIds.contains(photoId));
+      }
     }
 
     _logger.info('Filtered photoIds:');
     _logger.info(photoList);
 
     Future<List<String>> photoUrls = returnImageUrls(photoList);
+
     Map<String, String> imageIdUrlMap =
         _dataMapper.combineListsToMap(await photoUrls, photoList);
 
     return imageIdUrlMap;
   }
 
+  // USED FOR PRINTING IMAGES IN GALLERY // NO AUTHORIZATION NEEDED
   // Return the baseUrls of photos based on a list of imageIds
   // return: List of urls of images
   Future<List<String>> returnImageUrls(List<String> imageIds) async {
@@ -116,6 +124,89 @@ class PhotoRequests {
       } else {
         _logger.info('BaseUrls of filtered images:');
         _logger.info(filteredUrls);
+      }
+
+      return filteredUrls;
+    } else {
+      _logger.warning('Failed with status code: ${tokenResult.statusCode}');
+      throw Exception('Failed to fetch image URLs');
+    }
+  }
+
+  Future<List<String>> returnFilteredGooglePhotosUrlAuthorized() async {
+    AuthClient authClient = await getAuthClient();
+    List<String> photoList = await getAllPhotos();
+    List<String> albumList = await _albumService.getAlbumIds();
+
+    if (albumList.isEmpty) {
+      var response = await authClient.get(
+        Uri.parse('https://photoslibrary.googleapis.com/v1/mediaItems'),
+      );
+
+      var data = jsonDecode(response.body);
+      var photos = data['mediaItems'] as List;
+      photoList = photos.map((photo) => photo['id'] as String).toList();
+    } else {
+      for (String albumId in albumList) {
+        String jsonBody = jsonEncode({
+          "albumId": albumId,
+          "pageSize": 100,
+        });
+
+        var response = await authClient.post(
+          Uri.parse(
+              'https://photoslibrary.googleapis.com/v1/mediaItems:search'),
+          body: jsonBody,
+        );
+
+        if (response.statusCode != 200) {
+          _logger.warning('Failed to get albums: ${response.statusCode}');
+        }
+
+        var data = jsonDecode(response.body);
+        if (data['mediaItems'] == null) {
+          _logger.info('No media items found in the album: $albumId');
+          continue;
+        }
+
+        var photos = data['mediaItems'] as List;
+        var photoIds = photos.map((photo) => photo['id'] as String).toList();
+
+        // Removing photoIds that exist in photoList
+        photoList.removeWhere((photoId) => photoIds.contains(photoId));
+      }
+    }
+
+    _logger.info('Filtered photoIds:');
+    _logger.info(photoList);
+
+    Future<List<String>> googlePhotosUrl = returnImagePhotosUrl(photoList);
+
+    return googlePhotosUrl;
+  }
+
+  // USED FOR REDIRECTING THE USER TO GOOGLE PHOTOS // AUTHORIZED ONLY
+  Future<List<String>> returnImagePhotosUrl(List<String> imageIds) async {
+    AuthClient authClient = await getAuthClient();
+
+    var tokenResult = await authClient.post(
+        Uri.parse('https://photoslibrary.googleapis.com/v1/mediaItems:search'));
+
+    if (tokenResult.statusCode == 200) {
+      String jsonResponse = tokenResult.body;
+      Map<String, dynamic> data = jsonDecode(jsonResponse);
+      List<String> filteredUrls = [];
+
+      // Filtering based on image IDs
+      List<dynamic> mediaItems = data['mediaItems'] as List<dynamic>;
+      for (var photo in mediaItems) {
+        if (imageIds.contains(photo['id'])) {
+          filteredUrls.add(photo['productUrl']);
+        }
+      }
+
+      if (filteredUrls.isEmpty) {
+        _logger.warning('No images found with provided IDs');
       }
 
       return filteredUrls;
